4ed37238ebb65430ea3786db01224312adef001d
Katrina Swales <katrina@ApplesMacBook10.default>
2016-01-03 01:59:35 +0000

    First commit

---
+++ CardcasttoIrc/ircAnswer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CardcasttoIrc
+{
+	public class ircAnswer
+	{
+		public string type{ get { return "Answer"; }}
+		public string value{ get; set; }
+		public string keep { get { return "Yes"; }}
+		public int draw { get; set;}
+		public int pick { get { return 1; }}
+		public string source{get;set;}
+	}
+}
+
---
+++ CardcasttoIrc/ircQuestion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CardcasttoIrc
+{
+	public class ircQuestion
+	{
+		public ircQuestion ()
+		{
+		}
+	}
+}
+
